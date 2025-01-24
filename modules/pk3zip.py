df9fdf18d2dbd733d417c5d7a9554653bd7e7d25
zibonbadi <zibonbadi@users.noreply.github.com>
2025-01-24 11:49:04 +0100

    Add helper module PK3ZIP

---
+++ modules/pk3zip.py
@@ -0,0 +1,19 @@
+import os
+import zipfile
+
+def add_marker(marker, pk3):
+    with  zipfile.ZipFile(pk3, "w") as zfile:
+        zfile.writestr(marker, "")
+
+def copy_tree(srcdir, pk3, arcname):
+    with  zipfile.ZipFile(pk3, "w") as zfile:
+        srcroot = os.path.abspath(workdir)
+        for root,dirs,files in os.walk(srcroot):
+            zfile.write(root)
+            for file in files:
+                zfile.write(os.path.join(root,file))
+
+def copy_file(srcfile, pk3, arcname):
+    with  zipfile.ZipFile(pk3, "w") as zfile:
+        abssrc = os.path.abspath(srcfile)
+        zfile.write(abssrc, arcname)
