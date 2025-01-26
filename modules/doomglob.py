7a1ccf7d909bca704628a60e5669aedbee149483
zibonbadi <zibonbadi@users.noreply.github.com>
2025-01-26 21:27:43 +0100

    Add better output and errors to Doomglob

--- modules/doomglob.py
+++ modules/doomglob.py
@@ -5,14 +5,24 @@ class DuplicateLumpError(Exception):
 
 def find_lump(srcdir, lumpname):
     import os, glob
+    from pathlib import Path
     out = list()
+
+    if srcdir == None:
+        raise FileNotFoundError(f'doomglob.find_lump(): No srcdir given')
+    if lumpname == None:
+        raise FileNotFoundError(f'doomglob.find_lump(): No lumpname given')
+
     #searchstr = srcdir+"/**/"+lumpname
     #if lumpname[0] != '/':
         #searchstr = srcdir+lumpname
 
     #for path in glob.glob(searchstr, root_dir=srcdir):
     for path in glob.glob(lumpname+'*', root_dir=srcdir):
-        doomname = os.path.basename(path)[:8]
-        out.append( (doomname, path) )
+        doomname = Path(path).stem[:8]
+        arcpath = os.path.dirname(path)+'/'+doomname
+        out.append( (doomname, path, arcpath) )
 
-    return out # List of tuples (LUMPNAME, PATH)
+    return out # List of tuples (LUMPNAME, PATH, ARCPATH)
+
+
