0afc4e671018ce6240a9ae15cde8e29f4df86b12
zibonbadi <zibonbadi@users.noreply.github.com>
2025-01-29 01:26:39 +0100

    Properly detetct empty offsets

--- modules/doompic.py
+++ modules/doompic.py
@@ -153,7 +153,7 @@ class Picture():
 
     def set_offset(self, offset: str):
         import re
-        if offset == None:
+        if offset == None or re.match("\s*", offset):
             self.offsetX = 0
             self.offsetY = 0
             return (self.offsetX, self.offsetY)
