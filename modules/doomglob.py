6e021e1af697c2d754ca429d39072777b05950f4
zibonbadi <zibonbadi@users.noreply.github.com>
2025-02-07 13:48:34 +0100

    Doomglob: Filter out directories

--- modules/doomglob.py
+++ modules/doomglob.py
@@ -13,15 +13,12 @@ def find_lump(srcdir, lumpname):
     if lumpname == None:
         raise FileNotFoundError(f'doomglob.find_lump(): No lumpname given')
 
-    #searchstr = srcdir+"/**/"+lumpname
-    #if lumpname[0] != '/':
-        #searchstr = srcdir+lumpname
-
     #for path in glob.glob(searchstr, root_dir=srcdir):
     for path in glob.iglob('**/'+lumpname+'*', root_dir=srcdir, recursive=True):
         doomname = Path(path).stem[:8]
         arcpath = '/'+os.path.dirname(path).lstrip('/').rstrip('/')+'/'+doomname
-        out.append( (doomname, path, arcpath) )
+        if  Path(srcdir.rstrip('/')+'/'+path).is_file(): # Filter out directories
+            out.append( (doomname, path, arcpath) )
 
     return out # List of tuples (LUMPNAME, PATH, ARCPATH)
 
