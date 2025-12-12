310d19536352fb2cabd8b0b507f9ce06d049f734
Zibon Badi <45879878+zibonbadi@users.noreply.github.com>
2025-12-13 00:30:56 +0100

    (WIP) Add alternative COLORMAP and TINTTAB conversion
    The following conversion methods are expected to be supported:
    
    euclidean_rgb (what used to be)
    cylindrical_hsv
    conical_hsv
    cie76
    cie2000
    
    Some notes
    
    1. HSV-based color conversions currently produce off-colored outputs
    2. The options are currently not exposed to the PK3Makefile.
       This is deliberate until I figure out where the user should
       ideally be able to define these.
    3. Although useful for converting FLATs, FADEs, and Graphics,
       I will need to refactor their classes before they can properly
       use this setting.

--- src/pk3make/modules/doompic.py
+++ src/pk3make/modules/doompic.py
@@ -8,11 +8,8 @@ class Palette:
 
         # Colormath-based code commented out for future reference
         # Euclidean distance is 50x faster
-        """
-        from colormath2.color_objects import sRGBColor, LabColor
+        from colormath2.color_objects import sRGBColor, HSVColor, LabColor
         from colormath2.color_conversions import convert_color
-        from colormath2.color_diff import delta_e_cie2000
-        """
 
         with Image.open(filename).convert("RGB") as img:
             # When it don't fit we make it fit
@@ -25,47 +22,46 @@ class Palette:
                     pixel = rez_i.getpixel((x,y))
                     #self.colors.append(pixel) # Tuple (R,G,B)
 
-                    # Precalc color conversions to speed up rgb2index - UPDATE: No bc fuck colormath
-                    #px_srgb = sRGBColor(pixel[0], pixel[1], pixel[2], is_upscaled=True) # COLORMATH STUB
-                    #px_cielab = convert_color(px_srgb, LabColor) # COLORMATH STUB
+                    # Precalc color conversions to speed up rgb2index
+                    px_srgb = sRGBColor(pixel[0], pixel[1], pixel[2], is_upscaled=True)
+                    px_hsv = convert_color(px_srgb, HSVColor)
+                    px_cielab = convert_color(px_srgb, LabColor)
+                    
                     color_o = {
                         "id": y*height+x,
                         #"rgb": px_srgb, # COLORMATH STUB
                         "r": pixel[0],
                         "g": pixel[1],
                         "b": pixel[2],
-                        #"cielab": px_cielab, # COLORMATH STUB
+                        "h": px_hsv.hsv_h,  # range: [0.0,360.0)
+                        "s": px_hsv.hsv_s,  # range: [0.0,1.0]
+                        "v": px_hsv.hsv_v,  # range: [0.0,1.0]
+                        "cielab": px_cielab, # COLORMATH STUB
                     }
 
                     rgbcolor = (pixel[0] << 16) | (pixel[1] << 8) | (pixel[2])
                     self.colors.append(color_o) # Tuple (R,G,B)
                     self.color_lookup[rgbcolor] = color_o["id"]
 
-    def rgb2index(self, color: tuple):
-        # Colormath-based code commented out for future reference
-        # Euclidean distance is 50x faster
-        """
-        from colormath2.color_objects import sRGBColor, LabColor
-        from colormath2.color_conversions import convert_color
-        from colormath2.color_diff import delta_e_cie2000
-        """
+    def rgb2index(self, color: tuple, color_conversion_method=None):
+        
+        if color_conversion_method not in ["euclidean_rgb"]:
+            raise RuntimeError(f"RGB color conversion does not support method {color_conversion_method}. (How did we get here?)")       
+        
         # Hot path O(1): Color matches exactly (most common if you know what you're doing)
         rgbcolor = (int(color[0]) << 16) | (int(color[1]) << 8) | int(color[2])
+        
         if rgbcolor in self.color_lookup.keys():
             #print(f"Converting {color} => #{rgbcolor:X}")
             return self.colors[self.color_lookup[rgbcolor]]["id"]
 
         # Cold path: Linear search for the closest color
-        #color_lab = convert_color(sRGBColor(color[0], color[1], color[2], is_upscaled=True), LabColor)
         min_delta_e = float('inf')
         min_idx = -1
+
+        # Linear search w/ euclidean RGB distance
+        # About 50x faster than LAB-distance, but very inaccurate
         for icolor in self.colors:
-            """
-            #print(f"ICOLOR {index}: {icolor}")
-            #icolor_lab = convert_color(sRGBColor(icolor[0], icolor[1], icolor[2], is_upscaled=True), LabColor)
-            delta_e = delta_e_cie2000(color_lab, icolor["cielab"])
-            """
-            # Simple euclidean distance
             delta_e = ( \
                 (color[0]-icolor['r'])**2 + \
                 (color[1]-icolor['g'])**2 + \
@@ -76,8 +72,122 @@ class Palette:
                 min_idx = icolor["id"]
             if delta_e == 0: # Exact match, no need to continue
                 break
+
         #print(f"Found color {min_idx}:{self.colors[min_idx]} for image color {color}")
         return min_idx
+
+
+    def hsv2index(self, color: tuple, color_conversion_method=None):
+        
+        from colormath2.color_objects import AdobeRGBColor, HSVColor, LabColor
+        from colormath2.color_conversions import convert_color
+        from colormath2.color_diff import delta_e_cie2000
+        
+        if color_conversion_method not in ["cylindrical_hsv", "conical_hsv"]:
+            raise RuntimeError(f"HSV color conversion does not support method {color_conversion_method}. (How did we get here?)")
+        
+        # Hot path O(1): Color matches exactly (most common if you know what you're doing)
+        #color_lab = convert_color(HSVColor(color[0], color[1], color[2]), LabColor)
+        color_rgb = convert_color(HSVColor(color[0], color[1], color[2]), AdobeRGBColor)
+        rgbcolor = color_rgb.get_rgb_hex()
+
+        # Hot path O(1): Color matches exactly (most common if you know what you're doing)
+        if rgbcolor in self.color_lookup.keys():
+            #print(f"Converting {color} => #{rgbcolor:X}")
+            return self.colors[self.color_lookup[rgbcolor]]["id"]
+
+        # Cold path: Linear search for the closest color
+        min_delta_e = float('inf')
+        min_idx = -1
+
+        # Linear search w/ CIE 2000 LAB distance
+        for icolor in self.colors:
+            #print(f"ICOLOR {index}: {icolor}")
+
+            """
+            delta_e = ( \
+                # Brightness
+                (color[2]-icolor['r'])**2 + \
+                # Chroma (saturation*brightness)
+                (color[1]-icolor['g'])**2 + \
+                # Hue (circular)
+                (color[0]-icolor['b'])**2 \
+            )**(1/2)
+
+            delta_e = delta_e_cie2000(color_lab, icolor["cielab"])
+            """
+            
+            # Normalize all values to range(0,1), then euclidean distance over the cylinder
+            delta_e = ( \
+                # Brightness
+                (color[2]-icolor['v'])**2 + \
+                # Saturation ("cylindrical_hsv") or Chroma ("conical_hsv")
+                ( (color[1]-icolor['s'])**2
+                    if color_conversion_method == "cylindrical_hsv"
+                    else ((color[2]*color[1])-(icolor['v']*icolor['s']))**2 ) +
+                # Hue (circular distance)
+                ( min( abs(color[0]-icolor['h']), 360-abs(color[0]-icolor['h']) ) / 180.0 )**2 \
+            )**(1/2)
+
+            #delta_e = delta_e_cie2000(color_lab, icolor["cielab"])
+            
+            
+            if delta_e < min_delta_e:
+                min_delta_e = delta_e
+                min_idx = icolor["id"]
+            if delta_e == 0: # Exact match, no need to continue
+                break
+
+        return min_idx
+    
+
+    def lab2index(self, color: tuple, color_conversion_method=None):
+        # Colormath-based code commented out for future reference
+        # Euclidean distance is 50x faster
+        from colormath2.color_objects import AdobeRGBColor, LabColor
+        from colormath2.color_conversions import convert_color
+        from colormath2.color_diff import delta_e_cie2000, delta_e_cie1976
+        
+        if color_conversion_method not in ["cie76", "cie2000"]:
+            raise RuntimeError(f"L*a*b color conversion does not support method {color_conversion_method}. (How did we get here?)")
+
+        # Hot path O(1): Color matches exactly (most common if you know what you're doing)
+        color_rgb = convert_color(LabColor(color[0], color[1], color[2]), AdobeRGBColor)
+        rgbcolor = color_rgb.get_rgb_hex()
+        #rgbcolor = (int(color_rgb.rgb_r) << 16) | (int(color_rgb.rgb_g) << 8) | int(color_rgb.rgb_b)
+        
+        if rgbcolor in self.color_lookup.keys():
+            print(f"Converting {color} => #{rgbcolor:X}")
+            return self.colors[self.color_lookup[rgbcolor]]["id"]
+
+        # Cold path: Linear search for the closest color
+        #color_lab = convert_color(sRGBColor(color[0], color[1], color[2], is_upscaled=True), LabColor)
+        min_delta_e = float('inf')
+        min_idx = -1
+
+        color_lab = LabColor(color[0], color[1], color[2])
+
+        for icolor in self.colors:
+            #print(f"ICOLOR {index}: {icolor}")
+
+            delta_e = float("inf")
+
+            match color_conversion_method:
+                case "cie2000":
+                    delta_e = delta_e_cie2000(color_lab, icolor["cielab"])
+                case "cie76":
+                    delta_e = delta_e_cie1976(color_lab, icolor["cielab"])
+            
+            
+            if delta_e < min_delta_e:
+                min_delta_e = delta_e
+                min_idx = icolor["id"]
+            if delta_e == 0: # Exact match, no need to continue
+                break
+        
+        #print(f"Found color {min_idx}:{self.colors[min_idx]} for image color {color}")
+        return min_idx
+
 
     def generate_colormap(self):
         return
@@ -116,50 +226,135 @@ class Palette:
                 exbytes.append(int(b))
         return bytes(exbytes)
 
-    def colormap_tobytes(self):
-        """
-        from colormath.color_objects import HSVColor, sRGBColor
-        from colormath.color_conversions import convert_color
-        """
+
+    def colormap_tobytes(self, color_conversion_method="conical_hsv"):
+        from colormath2.color_objects import HSVColor, sRGBColor, LabColor
+        from colormath2.color_conversions import convert_color
+        from math import exp, log
 
         out = bytearray()
-        # Y/X coordinate loop because loop order matters
-        for c,v in [(c,v) for v in range(32) for c in range(256)]:
+        levels = 32 # Default amount of brightness levels. Might be adjustable in the future for non-Weissblatt projects
 
-            """
-            input_hsv = convert_color( sRGBColor( \
-                        self.colors[c][0], \
-                        self.colors[c][1], \
-                        self.colors[c][2] \
-                    , HSVColor)
-            output_hsv = ( input_hsv[0], input_hsv[1], input_hsv[2] * (1-v/32) )
-            output_rgb = sRGBColor()
+        match color_conversion_method:
+            case "euclidean_rgb":
+                
+                print("Using euclidean RGB interpolation")
 
-            out += self.rgb2index(output_rgb.value_tuple())
-            """
+                # Y/X coordinate loop because loop order matters
+                for c,v in [(c,v) for v in range(levels) for c in range(256)]:
+                    # Simple RGB squash
+                    brightness = ( \
+                                    self.colors[c]["r"] * (1-(v/(levels-1))), \
+                                    self.colors[c]["g"] * (1-(v/(levels-1))), \
+                                    self.colors[c]["b"] * (1-(v/(levels-1))) \
+                                )
+                    
+                    out += self.rgb2index(brightness, color_conversion_method=color_conversion_method).to_bytes(1)
 
-            # Simple RGB squash for now
-            # TODO: Add HSV/LAB-conversion after testing
-            brightness = ( \
-                            self.colors[c]["r"] * (1-(v/32)), \
-                            self.colors[c]["g"] * (1-(v/32)), \
-                            self.colors[c]["b"] * (1-(v/32)) \
-                        )
-            out += self.rgb2index(brightness).to_bytes(1)
+            case "cylindrical_hsv" | "conical_hsv":
+                
+                print("Using HSV interpolation")
+
+                hsv_black = HSVColor(240, 0.0, 0.0)
+
+                # Y/X coordinate loop because loop order matters
+                for c,v in [(c,v) for v in range(levels) for c in range(256)]: # HSV-based interpolation
+                    
+                    """
+                    input_hsv = convert_color( sRGBColor( \
+                                self.colors[c]["r"], \
+                                self.colors[c]["g"], \
+                                self.colors[c]["b"] \
+                                ), HSVColor)
+                    """
+                    
+                    
+                    """
+                    input_hsv.hsv_h,
+                    input_hsv.hsv_s,
+                    # Squash [0.0;255.0] to [0.0;1.0] and apply brightness fade
+                    (input_hsv.hsv_v/255.0) * (1.0-v/(levels-1))
+                    """
+
+                    # Simple linear brightness interpolation
+                    output_hsv = HSVColor(
+                                            #input_hsv.hsv_h * (1-v/(levels-1)) + hsv_black.hsv_h * v/(levels-1),
+                                            #(((hsv_black.hsv_h-self.colors[c]["h"]) + 180) % 360 - 180)*v/(levels-1),
+                                            self.colors[c]["h"],
+                                            self.colors[c]["s"] * (1-v/(levels-1)) + hsv_black.hsv_s * v/(levels-1),
+                                            self.colors[c]["v"] * (1-v/(levels-1)) + (hsv_black.hsv_v) * v/(levels-1)
+                                        )
+
+                    out += self.hsv2index(output_hsv.get_value_tuple(), color_conversion_method=color_conversion_method).to_bytes(1)
+
+            case "cie76" | "cie2000":
+                
+                print("Using CIELAB interpolation")
+
+                lab_black = convert_color(sRGBColor(0,0,0), LabColor)
+
+                # Y/X coordinate loop because loop order matters
+                for c,v in [(c,v) for v in range(levels) for c in range(256)]:
+                    # LAB conversion
+                    
+                    input_lab = self.colors[c]["cielab"]
+                    scale = v/(levels-1)
+                    
+                    # Actual LAB-based interpolation
+                    if v == 32: # Full dark
+                        print(f"RGB {(self.colors[c]["r"], self.colors[c]["g"], self.colors[c]["b"])} -> L*a*b color: {input_lab}\n")
+                    output_lab = LabColor(
+                                            input_lab.lab_l * (1-scale) + lab_black.lab_l * scale,
+                                            input_lab.lab_a * (1-scale) + lab_black.lab_a * scale,
+                                            input_lab.lab_b * (1-scale) + lab_black.lab_b * scale
+                                        )
+
+                    out += self.lab2index(output_lab.get_value_tuple(), color_conversion_method=color_conversion_method).to_bytes(1)
+
+
+            case _:
+                raise RuntimeError(f'Unknown color conversion method "{color_conversion_method}".')
+
         return out
 
-    def tinttab_tobytes(self, factor:float):
+    def tinttab_tobytes(self, factor:float, conversion="rgb"):
         if type(factor) != float or not (0 <= factor <= 1):
             raise RuntimeError(f"Invalid TINTTAB factor {factor}")
 
         out = bytearray()
-        for x,y in [(x,y) for x in range(256) for y in range(256)]:
-            tintcolor = ( \
-                            self.colors[x]["r"] * (1-factor) + self.colors[y]["r"] * factor, \
-                            self.colors[x]["g"] * (1-factor) + self.colors[y]["g"] * factor, \
-                            self.colors[x]["b"] * (1-factor) + self.colors[y]["b"] * factor \
-                        )
-            out += self.rgb2index(tintcolor).to_bytes(1)
+
+        match conversion:
+            case "rgb":
+                for x,y in [(x,y) for x in range(256) for y in range(256)]:
+
+                    tintcolor = ( \
+                                    self.colors[x]["r"] * (1-factor) + self.colors[y]["r"] * factor, \
+                                    self.colors[x]["g"] * (1-factor) + self.colors[y]["g"] * factor, \
+                                    self.colors[x]["b"] * (1-factor) + self.colors[y]["b"] * factor \
+                                )
+                    out += self.rgb2index(tintcolor).to_bytes(1)
+            case "hsv":
+                for x,y in [(x,y) for x in range(256) for y in range(256)]:
+
+                    tintcolor = ( \
+                                    # Linearly interpolate the angle of the hue circle
+                                    (((self.colors[y]["h"]-self.colors[x]["h"]) + 180) % 360 - 180)*factor, \
+                                    self.colors[x]["s"] * (1-factor) + self.colors[y]["s"] * factor, \
+                                    self.colors[x]["v"] * (1-factor) + self.colors[y]["v"] * factor \
+                                )
+                    out += self.hsv2index(tintcolor).to_bytes(1)
+            case "lab":
+                for x,y in [(x,y) for x in range(256) for y in range(256)]:
+
+                    tintcolor = ( \
+                                    self.colors[x]["cielab"].lab_l * (1-factor) + self.colors[y]["cielab"].lab_l * factor, \
+                                    self.colors[x]["cielab"].lab_a * (1-factor) + self.colors[y]["cielab"].lab_a * factor, \
+                                    self.colors[x]["cielab"].lab_b * (1-factor) + self.colors[y]["cielab"].lab_b * factor \
+                                )
+                    out += self.lab2index(tintcolor).to_bytes(1)
+            case _:
+                raise Exception(f"Invalid color conversion method \"{conversion}\". Check tinttab definition for <++>")
+            
         return out
 
 
